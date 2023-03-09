4deb258a22341e96a813e3f0b065f9c73b3b677e
FootballOnlooker <andrewtimofeev2013@gmail.com>
2023-03-09 23:27:11 +0100

    Fixed views

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
+
 def signupuser(request):
-    return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    if request.method == 'GET':
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
+    else:
+        # Create new user
+        User.objects.create_user(request.POST['username'], password=request.POST['password1'])
