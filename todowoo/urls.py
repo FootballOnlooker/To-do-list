18b96cf6f24e1f6243f34aa5c902898324586868
FootballOnlooker <andrewtimofeev2013@gmail.com>
2023-03-14 16:23:55 +0100

    Created login, logout and models

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #Auth
     path('signup/',views.signupuser, name='signupuser'),
+    path('logout/',views.logoutuser, name='logoutuser'),
+    path('login/',views.loginuser, name='loginuser'),
     #Todos
-     path('current/',views.currenttodos, name='currenttodos'),
+    path('',views.home, name='home'),
+    path('current/',views.currenttodos, name='currenttodos'),
 ]
